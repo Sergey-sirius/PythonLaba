a00b4802f1dc7a39eed36d5a9ffb2af5db5e08b2
Sergey <2hotab2@gmail.com>
2023-01-02 01:14:00 +0200

    Laba4 add book

--- Laba-4/laba4.py
+++ Laba-4/laba4.py
@@ -1,3 +1,4 @@
+import csv, glob, sys, os, linecache, filecmp
 
 # Task 1
 # Скласти випадкові фрази на основі трьох списків зі словами.
@@ -16,4 +17,43 @@
 # Кожен список не менше 10 слів
 # Підрахувати частоту появи цих слів в тексті
 # Розрахувати і зробити висновок якої емоції в тексті більше
+
+# тека з файлами книг
+pathbase = 'tstbook'
+
+def word_spliter(str):
+    result = []
+    word = ''
+    # chr(8212) '—' not '-'
+    for char in str:
+        if char in '-\",.;?! —' :
+            if word: result.append(word)
+            word = ''
+        else:
+            word += char
+    return result
+
+
+# Головна частина
+if __name__ == '__main__':
+    # Данні будуть розташовані в директорії  pathbase
+    os.chdir(pathbase)
+
+    #print(word_spliter("Lorem,\"ipsum;bingo.Bongo? King of Kongo."))
+    #print(toster_word_split("Lorem,\"ipsum;bingo.Bongo? King of Kongo."))
+
+    list_words = []
+    #f = open("palyty.txt", "r")
+
+    with open("palyty.txt") as file:
+        line = file.readline()
+        while line:
+            s=line.split("\n")
+            print(s[0])
+            if s[0] != '':
+                list_words.append(word_spliter(s[0]))
+            line = file.readline()
+
+    print("============================")
+    print(list_words)
 
