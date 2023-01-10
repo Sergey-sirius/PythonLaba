d5af8ca02736ec0dff06ad6801144b0683642cc9
Sergey <2hotab2@gmail.com>
2023-01-10 23:25:21 +0200

    6

---
+++ Laba-6/tst/tst1.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+r = requests.get("https://news.ycombinator.com/newest")
+#r = requests.get("https://translate.google.com.ua/?hl=uk#en/uk/python")
+
+print(r.status_code)
+
+if r.status_code == 200 :
+    print(r.text[: 100])
+    page = BeautifulSoup(r.text, 'html.parser')
+    print("============================================")
+    #print(page.text[: 2000])
+    #print(page.text)
+    #print(page.head.title.text)
+
+    tbl_list = page.findAll('table')
+    print(len(tbl_list))
+    #print(tbl_list)
+    print(page.table.text)
