4c69a3a3437c1580a15f546bcc6632735273020b
Sergey <2hotab2@gmail.com>
2023-01-20 22:39:13 +0200

    Лаба 6 test

--- Laba-6/mytest/requ1.py
+++ Laba-6/mytest/requ1.py
@@ -3,7 +3,51 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    pass
+    # откріваем сохраненную страничку
+    html = open('req.html').read()
+
+    #
+    soup = BeautifulSoup(html,'lxml')
+
+    #find возвращает объект первое найденое с условием
+    #div = soup.find('div', class_='links')
+    # OR
+    div = soup.find('div', {'class': 'links'})
+    print('1=========')
+    print(div)
+    #
+    link = div.find_all('a')
+    print('2=========')
+    print(link)
+
+    # FIND ALL
+    print('3=========')
+    link = soup.find_all('a')
+    for a in link:
+        print('------')
+        ref = a.get('href')
+        print(ref)
+        print(a)
+
+
+    print('4=========')
+    # Поиск вверх к родителю
+    # parent = find_parent() - find
+    # parents = find_parents() - find_all
+    h = soup.find('h1').find_parent('div', class_='two')
+    # print(h)
+
+    print('------')
+    h = soup.find('h1').find_parent('div', class_='one')
+    # print(h)
+
+    print('------')
+    h = soup.find('h1').find_parents('div')
+    print(h)
+
+    #
+
+    print('5=========')
 
 
 if __name__ == '__main__':
