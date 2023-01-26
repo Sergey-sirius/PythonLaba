dd8a7c6a256a345a1b2ee461e1918a441e5879df
Sergey <2hotab2@gmail.com>
2023-01-26 21:21:26 +0200

    lab6 1

--- Laba-6/laba6.py
+++ Laba-6/laba6.py
@@ -1,7 +1,66 @@
+import requests
+from random import choice
 
-# 1 відкрити сторінку з новинами (будь яку)
-# 2 читаємо весь текст в список
-# 3 підрахувати частоту появи слів в тексті
-# 4 підрахувати кількість html-тегів
-# 5 підрахувати кількість посиланнь
-# 6 підрахувати кількість зображень
+
+html_tag =[
+    '<!--...-->', '<a>', '<abbr>','<address>', '<applet>', '<area>', '<article>',
+    '<aside>', '<audio>', '<b>', '<base>', '<basefont>', '<bdo>', '<big>',
+    '<blockquote>', '<body>', '<br>', '<button>', '<canvas>', '<caption>', '<center>',
+    '<cite>', '<code>', '<col>', '<colgroup>', '<datalist>', '<dd>', '<del>',
+    '<details>', '<dfn>', '<dialog>', '<dir>', '<div>', '<dl>', '<!DOCTYPE>',
+    '<dt>', '<em>', '<embed>', '<fieldset>', '<figcaption>', '<figure>', '<font>', '<footer>',
+    '<form>', '<frame>', '<frameset>', '<h1>', '<h2>', '<h3>', '<h4>', '<h5>', '<h6>',
+    '<head>', '<header>', '<hgroup>', '<hr>', '<html>', '<i>', '<iframe>', '<img>',
+    '<input>', '<ins>', '<kbd>', '<keygen>', '<label>', '<legend>', '<li>', '<link>',
+    '<main>', '<map>', '<mark>', '<menu>', '<menuitem>', '<meta>', '<meter>', '<nav>',
+    '<noscript>', '<object>', '<ol>', '<optgroup>', '<option>', '<output>', '<p>',
+    '<param>', '<picture>', '<pre>', '<progress>', '<q>', '<rp>', '<rt>', '<ruby>',
+    '<s>', '<samp>', '<script>', '<section>', '<select>', '<small>', '<source>',
+    '<span>', '<strike>', '<strong>', '<style>', '<sub>', '<summary>', '<sup>', '<table>',
+    '<tbody>', '<td>', '<textarea>', '<tfoot>', '<th>', '<thead>', '<time>', '<title>',
+    '<tr>', '<track>', '<tt>', '<u>', '<ul>', '<var>', '<video>', '<wbr>'
+]
+
+desktop_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+
+# випадковий агент
+def random_headers():
+    return {'User-Agent': choice(desktop_agents),'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+
+def get_html(url, useragent=None, proxy=None):
+    # визначаємо який агент буде в запиті
+    useragent = random_headers()
+
+    #r = requests.get(url, headers=useragent, timeout=60)
+    #scraper = cloudscraper.create_scraper(delay=10, browser="chrome")
+    #r = scraper.get(url)
+
+    r = requests.get(url, headers=useragent, timeout = 60, proxies=proxy)
+    print(r.status_code)
+    return r.text
+
+def main():
+    #url = 'https://news.ycombinator.com/newest'
+    url = 'https://www.ukr.net'
+    #url = 'https://translate.google.com.ua/?hl=uk#en/uk/python'
+
+    # 1 відкрити сторінку з новинами (будь яку)
+    text_page = get_html(url)
+    # 2 читаємо весь текст в список
+    # 3 підрахувати частоту появи слів в тексті
+    # 4 підрахувати кількість html-тегів
+    # 5 підрахувати кількість посиланнь
+    # 6 підрахувати кількість зображень
+
+
+if __name__ == '__main__':
+    main()
--- Laba-6/mytest/requ1.py
+++ Laba-6/mytest/requ1.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
@@ -47,7 +48,64 @@ def main():
 
     # test5 v
     print('5=========')
+    text = soup.find('h1').next_element
+    print(text)
 
+    # test6 v
+    print('6=========')
+    text = soup.find('h1').next_sibling
+    print(text) #  пусто
+    text = soup.find('h1').next_sibling.next_sibling
+    print(text)  #<br/>
+    text = soup.find('h1').next_sibling.next_sibling.next_sibling
+    print(text)  #
+
+    print('7=========')
+    text = soup.find('h1').previous_sibling
+    print(text)  #
+
+    print('8=========')
+    div = soup.find('h1').parent
+    n = div.get('class')
+    print(n)
+
+
+    print('9= используем регулярные выражение ========')
+    a = soup.find('a', href=re.compile('bing.com'))
+    b = soup.find('a', href=re.compile('bing.com$'))
+    c = soup.find('a', text=re.compile('second'))
+    url1 = a.get('href')
+    url2 = b.get('href')
+    print("Выведет первую найденую",a)
+    print(url1)
+    print("Выведет вторую, у котоорой нет дополнительного пути",b)
+    print(url2)
+    print("Вывести первую найденую ссылку с текстом second",c)
+
+    print('10= используем регулярные выражение ========')
+    #12.04.2022
+    regexp = r'\d{2}.\d{2}.\d{4}'
+    a = soup.find('div', text=re.compile(regexp))
+    print("Вернет div с датой ",a)
+    print(a)
+
+    a = soup.find('div', text=re.compile('sss'))
+    print("Вернет div с текстом sss ", a)
+    print(a)
+
+    # пример
+    regexp = r'\w\-'   # буквы, цифры, _ ,знак -
+    regexp = r'/\d{1}$' # ищем строку с слешем и одной цифрой после которой ничего нет
+
+    regexp = r'^post'  # будет искать блок у которого с начала строки текст с словом post
+    a = soup.find('a', href=re.compile(regexp))
+    print("----- ", a)
+    print(a)
+
+    regexp = r'post'  #  текст с словом post
+    a = soup.find('a', href=re.compile(regexp))
+    print("----- ", a)
+    print(a)
 
 if __name__ == '__main__':
     main()
